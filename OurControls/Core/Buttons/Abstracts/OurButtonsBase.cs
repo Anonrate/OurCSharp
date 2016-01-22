33db9f5a0d0a350340c4e4342eae6d9a0dad60bd
Anonrate <Anonrate@outlook.com>
2016-01-22 15:33:52 -0500

    Added 'OurButtonBase' to Repository;

---
+++ OurControls/Core/Buttons/Abstracts/OurButtonsBase.cs
@@ -0,0 +1,239 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="OurButtonsBase.cs" company="OurCSharp">
+//     Copyright © 2016 OurCSharp
+// 
+//     This program is free software; you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation; either version 2 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+namespace OurCSharp.OurControls.Core.Buttons.Abstracts
+{
+    using System;
+    using System.ComponentModel;
+    using System.Drawing;
+    using System.Linq;
+    using System.Windows.Forms;
+
+    using OurCSharp.OurControls.Core.Buttons.Enums;
+    using OurCSharp.OurControls.Core.Buttons.Interfaces;
+    using OurCSharp.OurControls.Core.Buttons.Properties;
+
+    public abstract class OurButtonsBase : Control, IOurButtonBase
+    {
+        #region Fields
+        private OurOrientation _orientation = OurOrientation.Horizontal;
+        #endregion
+
+        #region Properties
+        public IOurButtonDesigner _ourDesigner { get; private set; }
+
+        [DefaultValue(typeof(Padding), "6, 2, 6, 2")]
+        public new Padding Padding { get { return base.Padding; } set { base.Padding = value; } }
+
+        [Browsable(false)]
+        public override Color BackColor
+        {
+            get { return base.BackColor; }
+            set
+            {
+                base.BackColor = value;
+                this.Invalidate();
+            }
+        }
+
+        [Browsable(false)]
+        public override Color ForeColor
+        {
+            get { return base.ForeColor; }
+            set
+            {
+                base.ForeColor = value;
+                this.Invalidate();
+            }
+        }
+
+        [Browsable(false)]
+        public override string Text
+        {
+            get { return base.Text; }
+            set
+            {
+                base.Text = value;
+                this.Invalidate();
+            }
+        }
+
+        [DefaultValue(true)]
+        [Description("This cannot be changed for rendering and visual appearance reasons.")]
+        protected override bool DoubleBuffered { get { return base.DoubleBuffered; } set { } }
+
+        public bool IsInDesignerMode => this.DesignMode;
+
+        [Category("OurButton")]
+        [DefaultValue(typeof(OurOrientation), "Horizontal")]
+        [Description("The Orientation of OurButton.")]
+        public OurOrientation Orientation
+        {
+            get { return this._orientation; }
+            set
+            {
+                this._orientation = value;
+                this.Size = this.UpdateAndGetMinimumSize();
+                this.Invalidate();
+            }
+        }
+
+        [Category("OurButton")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public IOurButtonDesigner Normal { get; }
+
+        [Category("OurButton")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public IOurButtonDesigner Hovered { get; }
+
+        [Category("OurButton")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public IOurButtonDesigner Clicked { get; }
+
+        [Category("OurButton")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
+        public IOurButtonDesigner Disabled { get; }
+
+        public override Size MinimumSize
+        {
+            get { return base.MinimumSize; }
+            set
+            {
+                base.MinimumSize = new Size(
+                    value.Width <= base.MinimumSize.Width ? base.MinimumSize.Width : value.Width,
+                    value.Height <= base.MinimumSize.Height ? base.MinimumSize.Height : value.Height);
+            }
+        }
+        #endregion
+
+        #region Constructors
+        protected OurButtonsBase()
+        {
+            base.DoubleBuffered = true;
+
+            this.Padding = new Padding(6, 2, 6, 2);
+
+            this._ourDesigner = this.Normal = new OurButtonNormal(this);
+
+            this.Hovered = new OurButtonHovered(this);
+            this.Clicked = new OurButtonClicked(this);
+            this.Disabled = new OurButtonDisabled(this);
+
+            this.SizeChanged += (sender, args) => this.Invalidate();
+        }
+        #endregion
+
+        #region Implementations
+        public void UpdateMinimumSize() { this.MinimumSize = this.UpdateAndGetMinimumSize(); }
+        #endregion
+
+        #region Methods
+        protected override void OnCreateControl()
+        {
+            this.Text = this.Normal.Text = this.Hovered.Text = this.Clicked.Text = this.Disabled.Text = this.Name;
+
+            // TODO If doesn't work, might have to use 'base'..
+            this.MinimumSize = this.UpdateAndGetMinimumSize();
+
+            this.BackColor = this._ourDesigner.BackColor;
+            this.ForeColor = this._ourDesigner.TextColor;
+        }
+
+        protected override void OnEnabledChanged(EventArgs e)
+        {
+            base.OnEnabledChanged(e);
+
+            this.BackColor = (this._ourDesigner = this.Enabled ? this.Normal : this.Disabled).BackColor;
+
+            this.UpdateColor();
+        }
+
+        private void UpdateColor()
+        {
+            this.ForeColor = this._ourDesigner.UseTextColor ? this._ourDesigner.TextColor : this.Normal.TextColor;
+            this.Text = this._ourDesigner.UseText ? this._ourDesigner.Text : this.Normal.Text;
+            this.Invalidate();
+        }
+
+        protected override void OnMouseEnter(EventArgs e)
+        {
+            base.OnMouseEnter(e);
+
+            this.BackColor = (this._ourDesigner = this.Enabled ? this.Hovered : this.Disabled).BackColor;
+
+            this.UpdateColor();
+        }
+
+        protected override void OnMouseLeave(EventArgs e)
+        {
+            base.OnMouseLeave(e);
+
+            this.BackColor = (this._ourDesigner = this.Enabled ? this.Normal : this.Disabled).BackColor;
+
+            this.UpdateColor();
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+
+            this.BackColor = (this._ourDesigner = this.Enabled ? this.Clicked : this.Disabled).BackColor;
+
+            this.UpdateColor();
+        }
+
+        protected override void OnMouseUp(MouseEventArgs e)
+        {
+            base.OnMouseUp(e);
+
+            this.BackColor = (this._ourDesigner = this.Enabled ? this.Normal : this.Disabled).BackColor;
+
+            this.UpdateColor();
+        }
+
+        private Size UpdateAndGetMinimumSize()
+        {
+            var minSizeF = SizeF.Empty;
+
+            foreach (var useText in new[]
+                                    {
+                                        new Tuple<bool, string>(this.Normal.UseText, this.Normal.Text),
+                                        new Tuple<bool, string>(this.Hovered.UseText, this.Hovered.Text),
+                                        new Tuple<bool, string>(this.Clicked.UseText, this.Clicked.Text),
+                                        new Tuple<bool, string>(this.Disabled.UseText, this.Disabled.Text)
+                                    }.Where(useText => useText.Item1))
+            {
+                SizeF sizeF;
+
+                minSizeF = (sizeF = this.CreateGraphics().MeasureString(useText.Item2, this.Font)).Width * sizeF.Height
+                           > minSizeF.Width * minSizeF.Height
+                               ? sizeF
+                               : minSizeF;
+            }
+
+            return
+                base.MinimumSize =
+                this._orientation == OurOrientation.Horizontal
+                    ? new SizeF(minSizeF.Width + this.Padding.Left + this.Padding.Top,
+                                minSizeF.Height + this.Padding.Bottom + this.Padding.Top).ToSize()
+                    : new SizeF(minSizeF.Height + this.Padding.Bottom + this.Padding.Top,
+                                minSizeF.Width + this.Padding.Left + this.Padding.Right).ToSize();
+        }
+        #endregion
+
+        ////private IOurButtonDesigner _ourDesigner;
+    }
+}
